9d461950e14a2e1e28679530d942dc4a490c4d23
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-03-17 14:41:15 +0530

    designing_a_component_for_your_entity_assignment_1

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,23 @@
-// write the component code here
+
+
+function Usercard(){
+    const profilePhoto = "https://www.w3schools.com/howto/img_avatar.png";
+    const name = "Mayank Sharma";
+    const mail = "sharmayank@mail.com";
+    const phoneNumber = "971878xxxx";
+    const address = "Haryana, India";
+
+
+    return(
+        <>
+        <div style={{ border: "1px solid white", padding: "2rem" }}>
+        <img src={profilePhoto} alt="image" style={{ width: "150px", height: "150px" }} />
+           <h2>Name: {name}</h2>
+           <p>Mail: {mail}</p> 
+           <p>Phone Number: {phoneNumber}</p> 
+           <p>Address: {address}</p> 
+        </div>
+        </>
+        )
+}
+export default Usercard
